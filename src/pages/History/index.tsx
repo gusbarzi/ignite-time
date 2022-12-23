462540499a32b2919b0bbd4b3bd261acd047a4cb
Gustavo Barzi <guhenriquebarzi@hotmail.com>
2022-12-23 17:53:24 -0300

    feature: history page and fixed bugs

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,52 @@
+import { HistoryContainer, HistoryList } from "./styles"
+
+export const History = () => {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <th>Tarefa</th>
+            <th>Duração</th>
+            <th>Início</th>
+            <th>Status</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há 2 meses</td>
+              <td>concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 Minutos</td>
+              <td>Há 2 meses</td>
+              <td>concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
