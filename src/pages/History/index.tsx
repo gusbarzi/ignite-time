493365c5726f10263a5b478cb88ded4e6ce521da
Gustavo Barzi <guhenriquebarzi@hotmail.com>
2023-01-03 21:20:55 -0300

    feature: add form validations

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles"
+import { HistoryContainer, HistoryList, Status } from "./styles"
 
 export const History = () => {
   return (
@@ -18,31 +18,41 @@ export const History = () => {
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há 2 meses</td>
-              <td>concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há 2 meses</td>
-              <td>concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há 2 meses</td>
-              <td>concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há 2 meses</td>
-              <td>concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 Minutos</td>
               <td>Há 2 meses</td>
-              <td>concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
           </tbody>
         </table>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import { Play } from "phosphor-react"
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 import { 
   HomeContainer, 
   FormContainer, 
@@ -7,15 +10,36 @@ import {
   StartCountdownButton,
   TaskInput,
   MinutesAmountInput,
-  } from './styles'
+} from './styles'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5).max(60)
+})
 
 export const Home = () => {
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
+
+  function handleCreateNewCycle(data: any) {
+
+  }
+
+  const task = watch('task');
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="">Vou trabalhar em</label>
-          <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto"/>
+          <TaskInput 
+            id="task"
+            list="task-suggestions" 
+            placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
+          />
 
           <datalist id="task-suggestions">
             <option value="projeto 1"></option>
@@ -32,6 +56,7 @@ export const Home = () => {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -46,7 +71,7 @@ export const Home = () => {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
